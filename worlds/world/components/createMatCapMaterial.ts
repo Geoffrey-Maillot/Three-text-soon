08a698261de940ef46611332fcac78981ac73977
Geoffrey Maillot <geoffrey.maillot@gmail.com>
2024-12-01 15:05:54 +0100

    fix : import matcap texture before use it

--- worlds/world/components/createMatCapMaterial.ts
+++ worlds/world/components/createMatCapMaterial.ts
@@ -3,10 +3,11 @@ import {
   MeshMatcapMaterialParameters,
   SRGBColorSpace,
 } from 'three';
+import matcapTexture from '../../../src/assets/textures/matcaps/10.png';
 import { loadTexture } from '../../../src/utils/textureLoader';
 
 const createMatcapMaterial = async (params?: MeshMatcapMaterialParameters) => {
-  const texture = await loadTexture('/src/assets/textures/matcaps/10.png');
+  const texture = await loadTexture(matcapTexture);
   texture.colorSpace = SRGBColorSpace;
 
   const material = new MeshMatcapMaterial({ matcap: texture, ...params });
